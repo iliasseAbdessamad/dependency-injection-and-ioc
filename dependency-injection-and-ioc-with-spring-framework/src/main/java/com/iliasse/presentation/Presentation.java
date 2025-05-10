cccbd16f0d9418ff872f017c8f1228cfacef3f1a
iliasseAbdessamad <samwillh7@gmail.com>
2025-05-10 20:40:58 +0100

    Injection de dépendence par instanciation dynamique

--- dependency-injection-and-ioc-with-spring-framework/src/main/java/com/iliasse/presentation/Presentation.java
+++ dependency-injection-and-ioc-with-spring-framework/src/main/java/com/iliasse/presentation/Presentation.java
@@ -1,18 +1,23 @@
 package com.iliasse.presentation;
 
 import com.iliasse.dao.IDao;
-import com.iliasse.dao.DaoImpl;
 import com.iliasse.metier.IMetier;
 import com.iliasse.metier.MetierImpl;
 
 public class Presentation {
 
-    public static void main(String[] args){
-        IDao dao = new DaoImpl();
+    public static void main(String[] args)
+    {
+        try{
+            //Instanciation dynamique
+            Class<IDao> clazz = (Class<IDao>)Class.forName("com.iliasse.dao.DaoImpl");
+            IDao dao = clazz.getConstructor().newInstance();
 
-        /*  Injection de la dépendence IDao par instanciation statique */
-        IMetier metier = new MetierImpl(dao);
-
-        System.out.println("La température est égale à : " + metier.calcul());
+            IMetier metier = new MetierImpl(dao);
+            System.out.println("La température est égale à : " + metier.calcul());
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
